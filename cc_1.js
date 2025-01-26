7af1083230c9e3b26e2b057508efb3fc8a2c7f75
Joshverde <52259302+Joshverde@users.noreply.github.com>
2025-01-26 17:59:07 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -2,3 +2,8 @@ let employeeName = "John Doe";
 const employeeID = 12345;
 var isActive = true;
 console.log (employeeName,typeof(employeeName),employeeID,typeof(employeeID),isActive,typeof(isActive))
+
+let productName = "Switch";
+const productPrice = 300;
+var isAvailable = true;
+console.log(productName,typeof(productName), productPrice, typeof(productPrice), isAvailable, typeof(isAvailable))
