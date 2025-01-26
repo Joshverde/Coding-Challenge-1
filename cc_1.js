3752541fc05369c4836ace33b06cbcbe1acb887d
Joshverde <52259302+Joshverde@users.noreply.github.com>
2025-01-26 17:56:22 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,4 @@
+let employeeName = "John Doe";
+const employeeID = 12345;
+var isActive = true;
+console.log (employeeName,typeof(employeeName),employeeID,typeof(employeeID),isActive,typeof(isActive))
